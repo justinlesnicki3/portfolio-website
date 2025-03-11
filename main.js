308e947264e3e99560e0e0bac4dee7e46a1a2f32
justinlesnicki3 <justinlesnicki5563@aol.com>
2025-03-10 20:35:16 -0500

    Fixed sidebar

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    hideSidebar(); 
+});
+
 function hideSidebar() {
     document.querySelector(".sidebar").style.display = "none";
 }
 
-// Attach the hideSidebar function to all sidebar links
 document.querySelectorAll(".sidebar li a").forEach(link => {
     link.addEventListener("click", hideSidebar);
 });
